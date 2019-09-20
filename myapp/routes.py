9ec1f770affe137717e9f5f0d1601dda95336918
Shiqiu2019 <snowchang02@gmail.com>
2019-09-20 09:51:45 +0800

    mkdir myapp

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,21 +1,19 @@
+from myapp import app
 from flask import Flask, render_template
 
-
-app = Flask(__name__)
-app.secret_key = 'secret 1 development key'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 @app.route('/Suzhou')
 def suzhou_index():
     return render_template('suzhou_index.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 @app.route('/Suzhou/test')
 def suzhou_test():
     return render_template('suzhou_test.html')
-
-if __name__ == '__main__':
-    app.run(debug = False)
